98bc67f369a82ce626c35bf50ce68645b7837d03
Bagrat <bagrat1996@gmail.com>
2021-12-08 16:23:03 +0400

    FAQ problem updated

--- FAQ page/index.js
+++ FAQ page/index.js
@@ -1,1 +1,11 @@
-const gago = 12;
+const questionList = [["question", "answer"]];
+
+const box = document.querySelector("#faq-box");
+const question = document.querySelector("#question")
+const answer = document.querySelector(".no-answer")
+question.innerText = questionList[0][0];
+answer.innerText = questionList[0][1];
+box.addEventListener("click", () => {
+  box.classList.toggle("extended");
+  answer.classList.toggle("no-answer");
+});
